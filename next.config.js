fa5a73174065072709c2e74b36602d8de78a41de
Hieu Tran <tranhieunz@gmail.com>
2022-02-20 20:48:02 +1300

    updated next config

--- next.config.js
+++ next.config.js
@@ -5,6 +5,13 @@ const nextConfig = {
     loader: "default",
     domains: ["localhost"],
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"]
+    });
+    return config;
+  }
 }
 
 module.exports = nextConfig
