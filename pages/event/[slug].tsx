b9c4de61de42b6d222ba2eae8aa9e330254cd1b2
Vincent <vincent@vuwunicodesjav1.vuw.ac.nz>
2022-05-14 20:26:20 +1200

    Updated: Changed gradient color blur placeholder

--- pages/event/[slug].tsx
+++ pages/event/[slug].tsx
@@ -39,14 +39,14 @@ const EventPage: NextPage<EventPageProps> = ({event, randomThreeEvents}) => {
   <svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
     <defs>
       <linearGradient id="g">
-        <stop stop-color="#333" offset="20%" />
-        <stop stop-color="#222" offset="50%" />
-        <stop stop-color="#333" offset="70%" />
+        <stop stop-color="#f7f7f725" offset="20%" />
+        <stop stop-color="#EEEEEE25" offset="50%" />
+        <stop stop-color="#f7f7f725" offset="70%" />
       </linearGradient>
     </defs>
-    <rect width="${w}" height="${h}" fill="#333" fillOpacity="0.25"/>
-    <rect id="r" width="${w}" height="${h}" fill="url(#g)" fillOpacity="0.25"/>
-    <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite" />
+    <rect width="${w}" height="${h}" fill="#f7f7f725" fillOpacity="0.25" />
+    <rect id="r" width="${w}" height="${h}" fill="url(#g)" fillOpacity="0.25" />
+    <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="2s" repeatCount="indefinite" />
   </svg>`;
 
   const toBase64 = (str: string) =>
