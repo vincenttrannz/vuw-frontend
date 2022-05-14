b97df3fa6cbc5ac04f966811b8e83b65b12a2b3e
Hieu Tran <tranhieunz@gmail.com>
2022-05-14 20:12:29 +1200

    Update: Remove BlurDataPlaceHolder function

--- pages/project/[slug].tsx
+++ pages/project/[slug].tsx
@@ -9,7 +9,6 @@ import { Container } from "react-bootstrap";
 import { fetchAPI } from "../../lib/api";
 import { Project, Projects } from '../../compilers/type';
 import { getStrapiMedia } from "../../lib/fetchData";
-import { convertImage, toBase64 } from '../functions/blurDataPlaceholder';
 import HeadData from "../components/HeadData";
 import TextDivider from '../components/views/TextDivider';
 import VicButton from '../components/views/VicButton';
@@ -29,6 +28,25 @@ type ProjectProps = {
 const ProjectPage: NextPage<ProjectProps> = ({project, randomThreeProjects}) => {
   const projectData = project.attributes;  
   const router = useRouter();
+
+  const convertImage = (w: number, h: number) => `
+  <svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <defs>
+      <linearGradient id="g">
+        <stop stop-color="#333" offset="20%" />
+        <stop stop-color="#222" offset="50%" />
+        <stop stop-color="#333" offset="70%" />
+      </linearGradient>
+    </defs>
+    <rect width="${w}" height="${h}" fill="#333" />
+    <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+    <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+  </svg>`;
+
+  const toBase64 = (str: string) =>
+    typeof window === 'undefined'
+      ? Buffer.from(str).toString('base64')
+      : window.btoa(str);
 
   const GoBack = () => {
     window.scrollTo({
